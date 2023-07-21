812ac36b0b2e5ec7b8a3ff2bbd5f9b64aa55b879
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 14:55:35 -0600

    display loading element if isLoading true from store

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,6 +2,9 @@ import { useSelector } from 'react-redux';
 const UsersList = () => {
   const { users, isLoading, error } = useSelector((state) => state.users);
 
+  if(isLoading) return <div className="loading">Loading...</div>
+  
+
   return (
     <div>
       <p>The user List will be placed here</p>
