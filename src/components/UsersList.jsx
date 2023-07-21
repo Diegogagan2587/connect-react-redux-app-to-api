b80d82b9671b0ae0ca62b160c93a395008700e45
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 14:34:39 -0600

    Create Component that will contain fetched userds

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,7 +1,10 @@
 const UsersList = () => {
+    console.log('this is the request',response);
   return (
     <div>
       <p>The user List will be placed here</p>
     </div>
   );
 };
+
+export default UsersList;
