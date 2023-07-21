e063189c6894c0e8eaeae8a3c604b05415fd9a51
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-20 20:55:41 -0600

    add users, isLoading and error elements within initialState

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,16 +1,22 @@
-import {  createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {};
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: undefined,
+};
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        fetchFromAPI: ()=>{
-            console.log('this should retrive the date form the API, this is a place holder')
-        }
-    }
-})
+  name: 'users',
+  initialState,
+  reducers: {
+    fetchFromAPI: () => {
+      console.log(
+        'this should retrive the date form the API, this is a place holder'
+      );
+    },
+  },
+});
 
 export const { fetchFromAPI } = usersSlice.actions;
 export default usersSlice.reducer;
