8d9b2b9586b9f1c263504fd68dc3bc2461aea461
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 14:47:16 -0600

    implement proper identation

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./users/usersSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from './users/usersSlice';
 
 const store = configureStore({
-    reducer: {
-        users: usersReducer,
-    },
+  reducer: {
+    users: usersReducer,
+  },
 });
 
 export default store;
