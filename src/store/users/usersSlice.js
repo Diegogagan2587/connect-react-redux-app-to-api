5ead8c858006943009f9487d10a8eb947b8be586
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 19:43:13 -0600

    pass erro to catchblock using rejectwithvalue

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,4 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const reduxAction = 'users/fetch';
+const fetchUser = async ()=>{
+  const urlAPI = 'https://randomuser.me/api/?results=5';
+  //this can contain the functoin fetch
+  console.log('this will fetch data promthe API')
+  try{
+    //code of block to try
+    const request = await fetch(urlAPI)
+    const response = await request.json();
+    console.log('the resons for fetchUser would be ----->',response)
+    return response;
+  }catch(error){
+    return rejectWithValue(error.message)
+  }
+}
+const getUsers = createAsyncThunk(reduxAction,fetchUser);
+
+fetchUser();//remove after debuggin
 
 const initialState = {
   users: [],
@@ -10,7 +30,11 @@ const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-  extraReducers: {},//probably wi wil fetch data from here;
+  extraReducers: (builder)=>{
+    builder.addCase(getUsers.fulfilled, (state, action) => {
+      console.log('will run if getUsers is fullfilerd');
+    })
+  }, //probably wi wil fetch data from here;
 });
 
 export const { fetchFromAPI, extraReducers } = usersSlice.actions;
