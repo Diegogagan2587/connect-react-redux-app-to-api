c4e1a4fffd328c35b3a3d717fbe7c0ef164afefe
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 20:00:49 -0600

    import and initialize use dispatch inside the component

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,10 +1,12 @@
 import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
 
 const User = ({user})=> <li>{user}</li>;
 
 const UsersList = () => {
   const { users, isLoading, error } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
   if(isLoading) return <div className="loading">Loading...</div>
   if(error) return <div className="loading">Somthing went wrong...</div>
