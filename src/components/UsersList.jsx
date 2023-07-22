ecfe22a2657294ea28890f748ad6cdd9606b1f23
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 20:04:03 -0600

    import useEffect and pass empty function and empty array as dependencie

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,22 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { useDispatch } from 'react-redux';
 
-const User = ({user})=> <li>{user}</li>;
+const User = ({ user }) => <li>{user}</li>;
 
 const UsersList = () => {
   const { users, isLoading, error } = useSelector((state) => state.users);
   const dispatch = useDispatch();
 
-  if(isLoading) return <div className="loading">Loading...</div>
-  if(error) return <div className="loading">Somthing went wrong...</div>
+  useEffect(()=>{},[])
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (error) return <div className="loading">Somthing went wrong...</div>;
 
   return (
     <ul>
-      {users.map((user)=><User 
-      key={uuidv4()}
-      user={user}
-      />)}
+      {users.map((user) => (
+        <User key={uuidv4()} user={user} />
+      ))}
     </ul>
   );
 };
