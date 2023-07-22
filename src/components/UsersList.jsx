f15ccf4929b02a52db4bc345aeaf21741e0632bd
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 22:50:55 -0600

    Reder fisrt and last name

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -3,19 +3,17 @@ import React, { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { getUsers } from '../store/users/usersSlice';
 
-const User = ({ user }) => <li>{user}</li>;
+const User = ({ user }) => <li>{user.name.first} {user.name.last}</li>;
 
 const UsersList = () => {
   const { users, isLoading, error } = useSelector((state) => state.users);
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getUsers());
-  }, [getUsers]);
+  }, []);
 
   if (isLoading) return <div className="loading">Loading...</div>;
   if (error) return <div className="loading">Something went wrong...</div>;
-
   return (
     <ul>
       {users.map((user) => (
