7b1ba649cd96136139a611f6877be13dc06eae50
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-21 20:42:00 -0600

    fix sintax errors

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,22 +1,22 @@
-import { createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import {} from '@reduxjs/toolkit';
 
 const reduxAction = 'users/fetch';
-const fetchUser = async ()=>{
+const fetchUser = async () => {
   const urlAPI = 'https://randomuser.me/api/?results=5';
   //this can contain the functoin fetch
-  console.log('this will fetch data promthe API')
-  try{
+  console.log('this will fetch data promthe API');
+  try {
     //code of block to try
-    const request = await fetch(urlAPI)
+    const request = await fetch(urlAPI);
     const response = await request.json();
-    console.log('the resons for fetchUser would be ----->',response)
+    console.log('the Response for fetchUser would be ----->', response);
     return response;
-  }catch(error){
-    return rejectWithValue(error.message)
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
-}
-const getUsers = createAsyncThunk(reduxAction,fetchUser);
+};
+export const getUsers = createAsyncThunk(reduxAction, fetchUser);
 
 const initialState = {
   users: [],
@@ -28,21 +28,19 @@ const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-  extraReducers: (builder)=>{
+  extraReducers: (builder) => {
     builder.addCase(getUsers.fulfilled, (state) => {
-      state.isLoading = false;
-    })
-    builder.addCase(getUsers.pending,(state)=>{
-      state.isLoading = true;
-    }),
-    builder.addCase(getUsers.rejected, ()=>{
+      return (state.isLoading = false);
+    });
+    builder.addCase(getUsers.pending, (state) => {
+      return (state.isLoading = true);
+    });
+    builder.addCase(getUsers.rejected, (state) => {
       state.isLoading = false;
       state.error = 'Something went wrong';
-    }),
+    });
   },
-  
 });
 
 export const { extraReducers } = usersSlice.actions;
 export default usersSlice.reducer;
-export getUsers;
